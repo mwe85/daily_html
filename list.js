9d05dc1c02d91abdc9f07c69b027b34c42a741f2
mwe85 <107491004+mwe85@users.noreply.github.com>
2022-09-07 15:21:58 -0400

    moved list stuff

--- list.js
+++ list.js
@@ -1,3 +1,44 @@
 class ListMaker{
     
 }
+
+class PageDataCollection{
+
+    sortOrder(){
+
+    }
+
+    createDOMTree(){
+
+    }
+}
+
+class PageData{
+    constructor(jsonObj){
+        this.index = jsonObj.main;
+        this.dir = jsonObj.dir;
+        this.order = jsonObj.order;
+    }
+
+    
+    get index(){
+        //lazily resolve to default
+        if(this.index === ""){
+            this.index = "index.html";
+            return this.index;
+        }
+    }
+
+    get dir(){
+        return this.dir;
+    }
+
+    get order(){
+        return this.order;
+    }
+}
+
+
+class PrototypeBuilder{
+    constructor(data. )
+}
